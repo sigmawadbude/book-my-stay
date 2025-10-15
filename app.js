74f1e00d384bee35c0735aedaa3fcaad1e0f61ef
Sigma wadbude <sigmawadbude@gmail.com>
2025-10-15 15:13:06 +0530

    feat: add endpoint to retrieve all hotels

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
+let hotels = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "./data/hotels.json"))
+);
 
+/**
+ * @swagger
+ * /api/v1/hotels:
+ *   get:
+ *     summary: Get all hotels.
+ *     description: Retrieve a list of all available hotels.
+ *     responses:
+ *       200:
+ *         description: A successful response with the list of hotels.
+ */
+app.get("/api/v1/hotels", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      hotels,
+    },
+  });
+});
 
 module.exports = app;
